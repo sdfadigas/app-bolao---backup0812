76948bcaf2f2413dcf9e5652aa954c9dcd56c5c6
sdfadigas <99447521+sdfadigas@users.noreply.github.com>
2022-12-15 17:51:50 -0300

    1512

--- js/ranking.js
+++ js/ranking.js
@@ -1,13 +1,30 @@
 
 const banco = firebase.firestore();
-//const user = localStorage.getItem("emailUser");
-const userName = localStorage.getItem("userName");
+const user = localStorage.getItem("emailUser");
 const palpites = banco.collection('palpites').doc(user)
 const resultadosTest = banco.collection('resultadosTest').doc("teste")
 
-palpites.get().then((doc)=>{
+
+
+const palpitesArray = [];
+const resultadosArray = [];
+
+palpites.get().then((doc) => {
   if (doc.exists) {
-    console.log(doc.data());
+    console.log('beleza')
+    palpitesArray.push(doc.data().palpites)
   }
+}).then((doc) => {
+  resultadosTest.get().then((doc) => {
+    if (doc.exists) {
+      console.log("ok");
+      resultadosArray.push(doc.data().resultadosTest)
+    }
+  }).then((doc) => {
+    if (palpitesArray[0] === resultadosArray[0]) {
+      console.log("okbeleo")
+    } else { console.log('errou') }
+  })
 })
+
 
