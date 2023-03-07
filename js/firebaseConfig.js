d2174becb32b16001e088199a0a0feb5f39fbcd1
Samara Fadigas <sdfadigas@gmail.com>
2023-03-07 14:31:45 -0300

    Update firebaseConfig.js
    add coments

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -1,3 +1,4 @@
+//Este é um código em JavaScript que define um objeto chamado firebaseConfig que contém as configurações necessárias para conectar o aplicativo à plataforma Firebase do Google. O objeto firebaseConfig possui várias propriedades que fornecem informações de configuração, como a chave da API, o nome de domínio de autenticação, o ID do projeto, o armazenamento em bucket, o ID do remetente de mensagens e o ID do aplicativo.
 const firebaseConfig = {
     apiKey: "AIzaSyDtAv9R6czPxg4HLX2qhsI7EqaxNVA9DL0",
     authDomain: "bolao-app-1b7a3.firebaseapp.com",
